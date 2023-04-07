0556b6476cdc722816a5e9845c882e695aff0a06
Animam <anicetsedogo@gmail.com>
2023-04-07 17:04:15 +0000

    Display, Delete record

--- appCRM/views.py
+++ appCRM/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
+from django.views.decorators.http import require_POST
 
 
 # Create your views here.
 def home(request):
     # check if the user is logged in
+    record = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -14,13 +17,13 @@ def home(request):
         users = authenticate(request, username=username, password=password)
         if users is not None:
             login(request, users)
-            messages.success(request, "You have been logged in")
+            messages.success(request, "You have logged in successfully")
             return redirect('home')
         else:
             messages.success(request, "There was an error, please try again...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'record': record})
 
 
 def login_user(request):
@@ -51,3 +54,26 @@ def register_user(request):
         return render(request, 'register.html', {'form': form})
 
     return render(request, 'register.html', {'form': form})
+
+
+def customize_record(request, pk):
+    if request.user.is_authenticated:
+        custo_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {"custo_record": custo_record})
+
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delet_record = Record.objects.get(id=pk)
+        if request.method == 'POST':
+            delet_record.delete()
+            messages.success(request, "Deleted successfully")
+            return redirect('home')
+        return render(request, 'delete_item.html')
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
