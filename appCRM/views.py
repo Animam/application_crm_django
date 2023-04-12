8947304f2b34e957266e211c3e51105c2ea2c181
Animam <anicetsedogo@gmail.com>
2023-04-12 17:46:18 +0000

    Add record , Update and the Final project

--- appCRM/views.py
+++ appCRM/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecord
 from .models import Record
-from django.views.decorators.http import require_POST
 
 
-# Create your views here.
+# Create your home views here.
 def home(request):
     # check if the user is logged in
     record = Record.objects.all()
@@ -30,12 +29,14 @@ def login_user(request):
     pass
 
 
+# Create your logout views here.
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('home')
 
 
+# Create your register views here.
 def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -56,6 +57,7 @@ def register_user(request):
     return render(request, 'register.html', {'form': form})
 
 
+# Create your customization  views here.
 def customize_record(request, pk):
     if request.user.is_authenticated:
         custo_record = Record.objects.get(id=pk)
@@ -66,14 +68,47 @@ def customize_record(request, pk):
         return redirect('home')
 
 
+# here is the view to delete individual record
 def delete_record(request, pk):
     if request.user.is_authenticated:
         delet_record = Record.objects.get(id=pk)
+
         if request.method == 'POST':
             delet_record.delete()
             messages.success(request, "Deleted successfully")
             return redirect('home')
-        return render(request, 'delete_item.html')
+        return render(request, 'delete_item.html', {'delet_record': delet_record})
     else:
         messages.success(request, "You must be logged in")
+        return redirect('home')
+
+
+# the add new record view is goes here
+def add_record(request):
+    form = AddRecord(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request, " Record saved successfully")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, " You must be logged in")
+        return redirect('home')
+
+
+# the update record view section
+def update_record(request, pk):
+    current_record = Record.objects.get(id=pk)
+    form = AddRecord(request.POST or None, instance=current_record)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, " Record has been updated")
+                return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, " You must be logged in")
         return redirect('home')
