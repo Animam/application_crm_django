f3d650d11d13ef39679d5c4f488d9e1e13861c0a
Animam <anicetsedogo@gmail.com>
2023-04-17 14:51:06 +0000

    Add record , Update and the Final project

--- app_django/settings.py
+++ app_django/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&!7=$9b9v=ivd^)_p06o&kp&0ud5!%)j8_zuxbdw0xf*&2^ik'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
